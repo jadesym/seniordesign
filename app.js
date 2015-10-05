32254eab39ce9bac2a495003679dd753ce405a8e
Kevin Fu <kevin.fu808@gmail.com>
2015-10-05 13:35:25 -0500

    added microcontroller endpoint

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var android = require('./routes/android');
+var microcontroller = require('./routes/microcontroller');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/android', android);
+app.use('/microcontroller', microcontroller)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/android.js
+++ routes/android.js
@@ -4,23 +4,38 @@ var Firebase = require("firebase");
 var ref = new Firebase("https://senior-design.firebaseio.com");
 var androidRef = ref.child("android");
 
+var num_times = 0;
+var sum_times = 0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('android');
+  res.render('index', { title: 'Android' });
 });
 
 /* GET users listing. */
-router.get('/sync', function(req, res, next) {
-  res.send('android');
+router.get('/sync-init', function(req, res, next) {
+  console.log("New get request to initialize syncing");
+  // Will now accept 100 get requests twice a second
+  num_times = 0;
+  times_array = [];
+  res.send('Syncing has been initialized!');
 });
 
 /* GET users listing. */
-router.get('/time', function(req, res, next) {
+router.get('/time/:androidTime', function(req, res, next) {
+  currentAndroidTime = req.params.androidTime;
   var time = process.hrtime();
-  res.send((time[0] * 1e9 + time[1].toString()));
+  var serverTimeInNanoseconds = time[0] * 1e9 + time[1];
+  var current_offset = serverTimeInNanoseconds - currentAndroidTime;
+  num_times += 1;
+  sum_times += current_offset;
+  res.send("Successfully received " + num_times.toString() + " times: " 
+    + currentAndroidTime.toString());
 });
 
+router.get('/get-offset', function(req, res, next) {
+  res.send("Average Offset Ping: " + (sum_times / num_times).toString())
+});
 
 /* GET users listing. */
 router.post('/xyz-data', function(req, res, next) {
