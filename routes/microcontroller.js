fa37921fe7046c0f71e835b871e0ab2c28c7673c
Kevin Fu <kevin.fu808@gmail.com>
2015-11-11 23:31:07 -0600

    added working time structure for firebase

--- routes/microcontroller.js
+++ routes/microcontroller.js
@@ -1,44 +1,92 @@
 var express = require('express');
 var router = express.Router();
-var Firebase = require("firebase");
+var Fireproof = require('fireproof'),
+  Firebase = require('firebase');
 var ref = new Firebase("https://senior-design.firebaseio.com");
 var microcontrollerRef = ref.child("microcontroller");
+// var moment = require('moment');
+var Q = require('q');
 
+Fireproof.bless(Q);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('microcontroller', { title: 'Microcontroller' });
 });
 
+function unixPureTime(timeOfRecording) {
+  var hours = timeOfRecording.getHours();
+
+  var minutes = timeOfRecording.getMinutes();
+  var seconds = timeOfRecording.getSeconds();
+  var milliseconds = timeOfRecording.getMilliseconds();
+  var resultUnix = hours;
+  console.log(hours + ":" + minutes + ":" + seconds + ":" + milliseconds);
+  resultUnix = resultUnix * 60 + minutes;
+  resultUnix = resultUnix * 60 + seconds;
+  resultUnix = resultUnix * 1000 + milliseconds;
+  return resultUnix;
+}
+
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-  var locationData = req.body;
-  console.log(locationData);
-  keyString = "";
-  valString = "";
-  for (var key in locationData) {
+  var rightNow = new Date();
+  console.log(rightNow.getTime());
+  // var currentSeconds = rightNow.getTime() / 1000;
+  // var momentNow = moment(currentSeconds);
+  // console.log(currentSeconds);
+  // console.log(momentNow.millisecond());
+  // console.log(momentNow.second());
 
-  	var key = key;
-  	keyString += key.toString();
-  	var val = locationData[key];
-  	valString += val.toString();
-  	var error = false;
+  var voltageData = req.body;
+  console.log(voltageData);
+  var returnString = "";
+  var failures = 0;
+  var successes = 0;
+  var allKeys = Object.keys(voltageData);
+  var lastKey = allKeys[allKeys.length - 1];
+  var totalAsyncCalls = [];
+  allKeys.forEach(function(key, index) {
+    // console.log("Entering for loop" + allKeys.toString());
+    var voltageValue = voltageData[key];
+    // console.log(key + voltageValue);
+    // var originalMoment = moment(currentSeconds);
+    console.log("Original Unix: " + rightNow.getTime());
+    var timeOfRecording = new Date(rightNow - key);
+    console.log("Original Unix: " + timeOfRecording.getTime());
+    var currentDay = (timeOfRecording.getTime() - timeOfRecording.getHours() * 60 * 60 * 1000
+      - timeOfRecording.getMinutes() * 60 * 1000 - timeOfRecording.getSeconds() * 1000 
+      - timeOfRecording.getMilliseconds()) / 1000;
+    var currentHour = timeOfRecording.getHours();
   	var checkError = function(error) {
   		if (error) {
-  			error = true;
-  			console.log("Data Not Sent Successfully!")
+        var errorString = key + "-" + voltageValue + ":Failed!\n";
+  			// console.log(errorString);
+        returnString += errorString;
+        failures += 1;
+        // console.log("Error " + failures);
   		} else {
-  			console.log("Data Successfully Put In DB!")
+        var successString = key + "-" + voltageValue + ":Success!\n";
+        // console.log(successString);
+        returnString += successString;
+        successes += 1;
+        // console.log("Success " + successes);
   		}
-  	};
-  	var keyRef = microcontrollerRef.child(key);
-  	keyRef.set(val, checkError);
-  }
-  if (error) {
-	res.send('Error: Data Not Put In Firebase' + keyString + ":" + valString);
-  } else {
-  	res.send("Success!" + keyString + ":" + valString);
-  }
+  	}
+    var justTheTime = unixPureTime(timeOfRecording); 
+  	var keyRef = microcontrollerRef.child(currentDay).child(currentHour).child(justTheTime),
+      fireproofKeyRef = new Fireproof(keyRef);
+    // console.log(failures +":" + successes);
+  	totalAsyncCalls.push(
+      fireproofKeyRef.set(voltageValue).then(
+        checkError
+      )
+    );
+  });
+  console.log("Number of Async Calls: " + totalAsyncCalls.length);
+  Q.allSettled(totalAsyncCalls).then(function(results) {
+    res.send("Successes: " + successes.toString() + " Failures: " + failures.toString() + "\n" + returnString);
+  });
 });
 
 
